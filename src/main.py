4d5f24304b26bf4c103999bcb67ff159a83460e3
zws <1156592100@qq.com>
2025-04-08 15:26:59 +0800

    解决后端跨域问题，修改了前端测试界面

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Response
 from fastapi.responses import StreamingResponse, JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 import cv2
 import threading
 from ultralytics import YOLO
@@ -13,6 +14,14 @@ from utils.window import Window
 from utils.reminder import reminder_bytes
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 允许所有来源，在生产环境中应该设置具体的域名
+    allow_credentials=True,
+    allow_methods=["*"],  # 允许所有方法
+    allow_headers=["*"],  # 允许所有头部
+)
+
 streaming = True
 lock = threading.Lock()
 
