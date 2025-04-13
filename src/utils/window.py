be06abace789c0c134b6f24312ff83dbc84c42bf
zws <1156592100@qq.com>
2025-04-13 18:23:33 +0800

    将window类data改成私有，再用装饰器方式返回深拷贝。完成将视频转换成数据集的方法的雏形

--- src/utils/window.py
+++ src/utils/window.py
@@ -3,21 +3,25 @@ import torch
 
 class Window:
     def __init__(self, device, window_size, frame_shape):
-        self.data = torch.zeros(window_size, *frame_shape, device=device)
+        self._data = torch.zeros(window_size, *frame_shape, device=device)
         self.window_size = window_size
         self.current_size = 0
 
     def add(self, x):
         assert self.current_size < self.window_size
-        self.data[self.current_size, :, :] = x
+        self._data[self.current_size, :, :] = x
         self.current_size += 1
         if self.current_size == self.window_size:
             return True
         return False
 
     def clear(self):
-        self.data[:] = 0
+        self._data.zero_()
         self.current_size = 0
+
+    @property
+    def data(self):
+        return self._data.clone().detach()
 
 
 if __name__ == '__main__':
